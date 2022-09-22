c4ec67206434ef984239fa896187ea614b1a6027
SarthakPaneru <sarthak.paneru007@gmail.com>
2022-09-22 16:01:24 +0545

    Hello world from html file

--- src/main/java/com/example/awaytofindpeace/AWayToFindPeaceApplication.java
+++ src/main/java/com/example/awaytofindpeace/AWayToFindPeaceApplication.java
@@ -2,12 +2,21 @@ package com.example.awaytofindpeace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Controller
 public class AWayToFindPeaceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AWayToFindPeaceApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		return "index";
 	}
 
 }
