2b54b15803b198df6d4ee08377e9bb1c09def701
SarthakPaneru <sarthak.paneru007@gmail.com>
2022-09-22 16:09:28 +0545

    Removed unnecesary code

--- src/main/java/com/example/awaytofindpeace/AWayToFindPeaceApplication.java
+++ src/main/java/com/example/awaytofindpeace/AWayToFindPeaceApplication.java
@@ -2,21 +2,12 @@ package com.example.awaytofindpeace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@Controller
 public class AWayToFindPeaceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AWayToFindPeaceApplication.class, args);
-	}
-
-	@GetMapping
-	public String hello() {
-		return "index";
 	}
 
 }
