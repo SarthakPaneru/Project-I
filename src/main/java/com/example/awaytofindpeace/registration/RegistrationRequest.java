fd6d992cd9301ac0b8e321978a5a3df7c0910256
SarthakPaneru <sarthak.paneru007@gmail.com>
2022-10-16 18:50:46 +0545

    initialized RegistrationRequest class

--- src/main/java/com/example/awaytofindpeace/registration/RegistrationRequest.java
+++ src/main/java/com/example/awaytofindpeace/registration/RegistrationRequest.java
@@ -1,4 +1,17 @@
 package com.example.awaytofindpeace.registration;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@RequiredArgsConstructor // Used against @AllArgsConstructor to remove error
+@Getter
+@EqualsAndHashCode
+@ToString
 public class RegistrationRequest {
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String password;
 }
