832d1e84b713499dd114066e644ae6115e590904
SarthakPaneru <sarthak.paneru007@gmail.com>
2022-10-16 18:52:46 +0545

    add Email validator class

---
+++ src/main/java/com/example/awaytofindpeace/registration/EmailValidator.java
@@ -0,0 +1,19 @@
+package com.example.awaytofindpeace.registration;
+
+import org.springframework.stereotype.Service;
+
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Service
+public class EmailValidator implements Predicate<String> {
+    @Override
+    public boolean test(String s) {
+        // Java email validation permitted by RFC 5322
+        String regexPattern = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+        Pattern pattern = Pattern.compile(regexPattern);
+        Matcher matcher = pattern.matcher(s);
+        return matcher.matches();
+    }
+}
