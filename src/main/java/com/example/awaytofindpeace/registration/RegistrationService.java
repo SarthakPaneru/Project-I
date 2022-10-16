4f089834ab190c515bf7e44450823f3484de4684
SarthakPaneru <sarthak.paneru007@gmail.com>
2022-10-16 19:02:52 +0545

    refactor indentation

--- src/main/java/com/example/awaytofindpeace/registration/RegistrationService.java
+++ src/main/java/com/example/awaytofindpeace/registration/RegistrationService.java
@@ -4,15 +4,7 @@ import lombok.*;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor // Used against @AllArgsConstructor to remove error
-@Getter
-@EqualsAndHashCode
-@ToString
 public class RegistrationService {
-//    private final String firstName;
-//    private final String lastName;
-//    private final String email;
-//    private final String password;
 
     public String register(RegistrationRequest request) {
         return "works";
