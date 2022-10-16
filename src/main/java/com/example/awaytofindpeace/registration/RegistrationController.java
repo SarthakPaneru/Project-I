56d75e05a52d8db70680add813d591419d168057
SarthakPaneru <sarthak.paneru007@gmail.com>
2022-10-16 18:19:52 +0545

    refactor code

--- src/main/java/com/example/awaytofindpeace/registration/RegistrationController.java
+++ src/main/java/com/example/awaytofindpeace/registration/RegistrationController.java
@@ -1,12 +1,12 @@
 package com.example.awaytofindpeace.registration;
 
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(path = "api/v1/registration")
 @AllArgsConstructor
 public class RegistrationController {
@@ -15,6 +15,7 @@ public class RegistrationController {
 
     @PostMapping
     public String register(@RequestBody RegistrationRequest request) {
+
         return registrationService.register(request);
     }
 }
