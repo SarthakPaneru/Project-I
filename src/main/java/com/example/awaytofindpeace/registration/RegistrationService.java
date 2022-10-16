b0caf2c7c1ab85137c1950db6664937364e1a962
SarthakPaneru <sarthak.paneru007@gmail.com>
2022-10-16 13:58:55 +0545

    Used proper lombok annotation

--- src/main/java/com/example/awaytofindpeace/registration/RegistrationService.java
+++ src/main/java/com/example/awaytofindpeace/registration/RegistrationService.java
@@ -1,19 +1,18 @@
 package com.example.awaytofindpeace.registration;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor // Used against @AllArgsConstructor to remove error
 @Getter
-@AllArgsConstructor
 @EqualsAndHashCode
 @ToString
 public class RegistrationService {
-    private final String firstName;
-    private final String lastName;
-    private final String email;
-    private final String password;
+//    private final String firstName;
+//    private final String lastName;
+//    private final String email;
+//    private final String password;
 
     public String register(RegistrationRequest request) {
         return "works";
