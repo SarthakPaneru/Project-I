1509f3676590e85208124adcea973cdbeefd1aa3
SarthakPaneru <sarthak.paneru007@gmail.com>
2022-10-17 11:47:10 +0545

    use signup method

--- src/main/java/com/example/awaytofindpeace/registration/RegistrationService.java
+++ src/main/java/com/example/awaytofindpeace/registration/RegistrationService.java
@@ -1,12 +1,33 @@
 package com.example.awaytofindpeace.registration;
 
+import com.example.awaytofindpeace.appUser.AppUser;
+import com.example.awaytofindpeace.appUser.AppUserRole;
+import com.example.awaytofindpeace.appUser.AppUserService;
 import lombok.*;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
 
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail) {
+            throw new IllegalStateException("Email not valid");
+        }
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER,
+                        false,
+                        true
+                )
+        );
     }
 }
