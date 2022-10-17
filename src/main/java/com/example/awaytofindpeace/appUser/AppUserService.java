b0ccd22e06bff45d3b29205f8e906fb2a5dbf76f
SarthakPaneru <sarthak.paneru007@gmail.com>
2022-10-17 17:17:39 +0545

    introduce token feature

--- src/main/java/com/example/awaytofindpeace/appUser/AppUserService.java
+++ src/main/java/com/example/awaytofindpeace/appUser/AppUserService.java
@@ -1,11 +1,16 @@
 package com.example.awaytofindpeace.appUser;
 
+import com.example.awaytofindpeace.registration.token.ConfirmationToken;
+import com.example.awaytofindpeace.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -14,6 +19,7 @@ public class AppUserService implements UserDetailsService {
     private final static String USER_NOT_FOUND_MSG = "user with %s not found";
     private final AppUserRepository appUserRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
     @Override
     public UserDetails loadUserByUsername(String email)
             throws UsernameNotFoundException {
@@ -37,6 +43,20 @@ public class AppUserService implements UserDetailsService {
 
         appUserRepository.save(appUser);
 
-        return "it works";
+        // Send Confirmation token
+        String token = UUID.randomUUID().toString();
+
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                appUser
+        );
+
+        confirmationTokenService.saveConfirmationToken(
+                confirmationToken
+        );
+
+        return token;
     }
 }
